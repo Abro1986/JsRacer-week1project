edfd5642e3698d3197e04b0f070c61fb76053e65
Abro1986 <AndrewBroestl@gmail.com>
2018-03-26 13:25:06 -0600

    It doesn't look great but it does what it needs too.

--- app.js
+++ app.js
@@ -3,35 +3,49 @@
 //Move each player towards the finish line by pressing a specific key (ex. right arrow for player one, 'd' for player 2).
 //Let the players know the race is over when one player has crossed the finish line
 
-let rainbow = document.getElementById('rainbow');
+const rainbow = document.getElementById('rainbow');
 
 const brown = document.getElementById('brown');
 
+const finish = document.getElementById("finish")
 
-console.log(rainbow)
 
 
+// Set some starting points for the trout
 let brownRight = 0
 let rainbowRight = 0
 
-function anim(e) {
-if(e.keyCode == 68) {
-	brownRight += 5;
+//created a function that takes an argument key and moves pieces 5px, and checks for winners
+function swim(key) {
+if(key.keyCode == 68) {
+	brownRight += 15;
 	brown.style.left = brownRight + "px";
 
-} else if(e.keyCode == 39) { 
-		rainbowRight += 5;
-		rainbow.style.left = rainbowRight + "px"
-	}
+} if(key.keyCode == 39) { 
+		rainbowRight += 15;
+		rainbow.style.left = rainbowRight + "px";
+		
+
+} if (rainbowRight >= 950) {
+		alert("Rainbow trout wins!");
+
+} if (brownRight >= 950) 
+		alert("Brown trout wins!")
 }
+// creating an event listener on the document to listen for keydowns
+document.onkeyup = swim;
 
-document.onkeydown = anim;
 
-//function rainbo(e) {
-//	if(e.keyCode == 38) { 
-//		rainbowRight += 5;
-//		rainbow.style.left = rainbowRight + "px"
-//	}
-//}
 
-//document.onkeydown = rainbo;
+
+
+
+
+
+
+
+
+
+
+
+
